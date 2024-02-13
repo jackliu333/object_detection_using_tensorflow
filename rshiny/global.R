65249e375ea51154b1a3f88923787d20ea7af380
jackliu333 <jackliu333@gmail.com>
2024-02-13 14:01:27 +0800

    update

--- rshiny/global.R
+++ rshiny/global.R
@@ -2,7 +2,7 @@ library(dplyr)
 library(readr)
 
 # df = read_csv("../video_recognition/master_list.csv")
-df = read_csv("/Users/liupeng/Documents/GitHub/object_detection_using_tensorflow/images_combined/master_list.csv")
+df = read_csv("../NN_model/master_list.csv")
 all_labels = unique(df$label)
 all_prdtypes = unique(df$ProductType)
 all_weights = unique(df$Weight)
